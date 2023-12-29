ae34df8d3942b15f794c435941cb483dfce497f3
Binny V A <binnyva@gmail.com>
2023-12-29 17:35:23 +0000

    Add search to header menu as well

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const handleHamburgerMenuClick = e => {
+    const target_id = e.currentTarget.getAttribute('data-target')
+    const target = document.getElementById(target_id)
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    e.currentTarget.classList.toggle('is-active')
+    target.classList.toggle('is-active')
+}
