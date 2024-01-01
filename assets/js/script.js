fe1e10f3067a32bfad8bbc663c4fb074fecd7f64
Binny V A <binnyva@gmail.com>
2024-01-01 17:14:17 +0000

    Add auto dark mode detection

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,14 @@ const handleHamburgerMenuClick = e => {
 function themeToggle() {
     document.body.classList.toggle("dark");
 }
+
+function init() {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.remove("light");
+        document.body.classList.add("dark");
+    } else {
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
+    }
+}
+window.onload=init;
